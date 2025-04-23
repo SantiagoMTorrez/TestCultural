5aeaebde78a6ceeeeff64ea1eb340e755c3f5fa0
OSM <andrewing.fis@gmail.com>
2025-04-23 19:01:47 -0400

    Aprendiendo a hacer componenets

---
+++ frontend/test-cultural/src/components/Clusterfuck.vue
@@ -0,0 +1,31 @@
+<template>
+    <canvas ref="canvas" id="canvas"></canvas>
+</template>
+
+<script>
+    import { ref, onMounted } from 'vue';
+export default {
+    name: "ClusterFuck",
+    setup(){
+        const canvas = ref(null);
+        const context = ref(null);
+        onMounted(() => {
+            context.value = canvas.value.getContext("2d")
+        })
+        return {canvas}
+    }
+}
+</script>
+
+<style>
+    *, *::after, ::before{
+        box-sizing: content-box;
+        padding: 0px;
+        margin: 0px;
+    }
+
+    #canvas{
+        height: 100vh;
+        width: 1000vw;
+    }
+</style>
